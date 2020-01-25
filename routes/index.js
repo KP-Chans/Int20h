cfd4d6921e36939a6d51f5bb82107aab50508a9f
arcanit33 <sasha.nikitin7009@gmail.com>
2020-01-25 17:05:26 +0200

    front

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const request = require("request");
-const axios   = require('axios');
-const router  = express.Router();
+const axios = require('axios');
+const router = express.Router();
 
-router.get('/', function (req, res) {
+router.get('/', function(req, res) {
 
     res.render('index', {
         trackId: req.query.id
+
     });
 })
 
@@ -14,14 +15,14 @@ router.post('/lyrics', function(req, ress) {
 
     var data = {
         'q': req.body.lyrics,
-        'api_token': '080f948aefc35f2fbd64c7205fcc5c14'
+        'api_token': 'f7114b9b152071e0523cd18154b88f58'
     };
 
     request({
         uri: 'https://api.audd.io/findLyrics/',
         form: data,
         method: 'POST'
-    }, function (err, res, body) {
+    }, function(err, res, body) {
         console.log(JSON.parse(body).result[0].title);
         let a = "https://api.deezer.com/search/track/?q=" + JSON.parse(body).result[0].title + "&index=0&limit=2&output=json";
         axios.get(a, "")
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express    = require('express');
-const mustache   = require('mustache-express');
-const path       = require('path');
+const express = require('express');
+const mustache = require('mustache-express');
+const path = require('path');
 const bodyParser = require('body-parser');
 const routeIndex = require('./routes/index');
 
@@ -8,6 +8,7 @@ const app = express();
 const port = 3000;
 const viewsDir = path.join(__dirname, 'views');
 
+app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser()); // get information from html forms
@@ -17,7 +18,6 @@ app.engine('mst', mustache(viewsDir));
 app.set('views', viewsDir);
 app.set('view engine', 'mst');
 
-app.use(express.static('public'));
 app.use('/', routeIndex);
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
