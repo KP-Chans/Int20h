4d5fba7a4a5217e0751771fa6c673832f3c53f14
Dmytro Leshchenko <dima.leshchenko20@gmail.com>
2020-01-25 19:42:20 +0200

    it works

---
+++ public/javascripts/index.js
@@ -0,0 +1,72 @@
+function getFormData(object) {
+    const formData = new FormData();
+    Object.keys(object).forEach(key => formData.append(key, object[key]));
+    return formData;
+}
+
+const recordAudio = () =>
+    new Promise(async resolve => {
+        const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+        const mediaRecorder = new MediaRecorder(stream);
+        const audioChunks = [];
+
+        mediaRecorder.addEventListener("dataavailable", event => {
+            audioChunks.push(event.data);
+        });
+
+        const start = () => mediaRecorder.start();
+
+        const stop = () =>
+            new Promise(resolve => {
+                mediaRecorder.addEventListener("stop", () => {
+                    const audioBlob = new Blob(audioChunks, { type: 'audio/mpeg-3' });
+                    const audioUrl = URL.createObjectURL(audioBlob);
+
+                    var data = new FormData();
+                    data.append('file', audioBlob);
+                    data.append('api_token', '080f948aefc35f2fbd64c7205fcc5c14')
+                    $.ajax({
+                        url: "https://api.audd.io/",
+                        type: 'POST',
+                        api_token: '080f948aefc35f2fbd64c7205fcc5c14',
+                        data: data,
+                        contentType: false,
+                        processData: false,
+                        success: function (data) {
+                            fetch('/audio', {
+                                method: 'POST',
+                                headers: {
+                                    'Content-Type': 'application/json',
+                                },
+                                body: JSON.stringify(data),
+                            })
+                            .then(x => x.json())
+                            .then(function(data) {
+                                console.log(data);
+                                window.location.replace("/?id=" + data);
+                            })
+                            .catch(err => console.log(err))
+                        }
+                    });
+                    const audio = new Audio(audioUrl);
+                    const play = () => audio.play();
+                    resolve({ audioBlob, audioUrl, play });
+                });
+
+                mediaRecorder.stop();
+            });
+
+        resolve({ start, stop });
+    });
+
+const sleep = time => new Promise(resolve => setTimeout(resolve, time));
+
+const handleAction = async () => {
+    const recorder = await recordAudio();
+    const actionButton = document.getElementById('action');
+    actionButton.disabled = true;
+    recorder.start();
+    await sleep(10000);
+    const audio = await recorder.stop();
+    actionButton.disabled = false;
+}
--- routes/index.js
+++ routes/index.js
@@ -1,54 +1,50 @@
 const express = require('express');
 const request = require("request");
-const axios   = require('axios');
-const fs      = require('fs');
-const router  = express.Router();
+const axios = require('axios');
+const fs = require('fs');
+const router = express.Router();
 
-router.get('/', function (req, res) {
+router.get('/', function(req, res) {
 
     res.render('index', {
         trackId: req.query.id
+
     });
 })
 
 router.post('/lyrics', function(req, ress) {
-
-    console.log(req.body.lyrics);
-    console.log(req.body.url);
-
-    //var data = {
-        //'q': req.body.lyrics,
-        //'api_token': '080f948aefc35f2fbd64c7205fcc5c14'
-    //};
-
-    // request({
-    //     uri: 'https://api.audd.io/findLyrics/',
-    //     form: data,
-    //     method: 'POST'
-    // }, function (err, res, body) {
-    //     console.log(JSON.parse(body).result[0].title);
-    //     let a = "https://api.deezer.com/search/track/?q=" + JSON.parse(body).result[0].title + "&index=0&limit=2&output=json";
-    //     axios.get(a, "")
-    //         .then(x => {
-    //             console.log(x.data.data[0].id);
-    //             ress.redirect('/?id=' + x.data.data[0].id);
-    //         })
-    //         .catch(err => console.log(err))
-    // });
-
     var data = {
-        'file': fs.createReadStream('/home/dima/Музыка/21 Savage/Issa Album/01 - Famous.mp3'),
-        'return': 'deezer',
+        'q': req.body.lyrics,
         'api_token': '080f948aefc35f2fbd64c7205fcc5c14'
     };
-    
+
     request({
-        uri: 'https://api.audd.io/',
+        uri: 'https://api.audd.io/findLyrics/',
         formData: data,
         method: 'POST'
-      }, function (err, res, body) {
-        console.log(body);
+    }, function(err, res, body) {
+        console.log(JSON.parse(body).result[0].title);
+        let a = "https://api.deezer.com/search/track/?q=" + JSON.parse(body).result[0].title + "&index=0&limit=2&output=json";
+        axios.get(a, "")
+            .then(x => {
+                console.log(x.data.data[0].id);
+                ress.redirect('/?id=' + x.data.data[0].id);
+            })
+            .catch(err => console.log(err))
     });
+})
+
+router.post('/audio', function(req, res) {
+    console.log(req.body);
+    let a = "https://api.deezer.com/search/track/?q=" + req.body.result.title + "&index=0&limit=2&output=json";
+    axios.get(a, "")
+        .then(x => {
+            console.log(x.data.data[0].id);
+   
+            res.json(x.data.data[0].id);
+        })
+        .catch(err => console.log(err))
+
 })
 
 module.exports = router;
